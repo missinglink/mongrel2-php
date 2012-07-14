5647777d45db00d7d49020292240d6b0d7299ab5
Peter Johnson <insomnia@rcpt.at>
2012-07-14 21:11:44 +0100

    Improve tests & coverage

--- test/Http/RequestTest.php
+++ test/Http/RequestTest.php
@@ -23,6 +23,43 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     }
     
     /**
+     * @covers \Mongrel\Http\Request::__construct
+     */
+    public function testConstructor()
+    {
+        $mockMongrelRequest = $this->getMockBuilder( 'Mongrel\Request' )
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $mockMongrelRequest->expects( $this->exactly( 1 ) )
+            ->method( 'getHeaders' )
+            ->will( $this->returnValue( array( 'a' => 'b' ) ) );
+        
+        $request = new Request( $mockMongrelRequest );
+        
+        $this->assertAttributeInstanceOf( '\Mongrel\Http\Headers', 'headers', $request );
+        $this->assertAttributeInstanceOf( '\HttpQueryString', 'query', $request );
+    }
+    
+    /**
+     * @covers \Mongrel\Http\Request::__construct
+     */
+    public function testConstructor_ParseQueryKey()
+    {
+        $mockMongrelRequest = $this->getMockBuilder( 'Mongrel\Request' )
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $mockMongrelRequest->expects( $this->exactly( 1 ) )
+            ->method( 'getHeaders' )
+            ->will( $this->returnValue( array( 'QUERY' => 'foo=bar&bar=baz' ) ) );
+        
+        $request = new Request( $mockMongrelRequest );
+        
+        $this->assertEquals( 'foo=bar&bar=baz', $request->getQuery()->get() );
+    }
+    
+    /**
      * @covers \Mongrel\Http\Request::getMongrelRequest
      */
     public function testGetMongrelRequest()
